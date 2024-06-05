19e13542eecec772163295b30423f1b4844c71c1
haseebismail703 <haseebismail703@gmail.com>
2024-06-06 00:48:01 +0500

    new

--- Backend/Routes/main.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import user from './user.js'
-import upload from './upload.js'
-const router = express.Router()
-
-router.use('/user',user)
-router.use('/upload',upload)
-export default router 

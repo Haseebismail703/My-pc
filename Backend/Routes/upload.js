f337a0638565c3976d827e6f77f30e446e52ecb0
haseebismail703 <haseebismail703@gmail.com>
2024-06-12 16:27:15 +0500

    new

--- Backend/Routes/upload.js
+++ Backend/Routes/upload.js
@@ -1,7 +1,21 @@
 import express from 'express'
 import multer from 'multer'
 let router = express.Router()
-const upload = multer({ dest: 'image/' })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'image/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()
+     * 1E9)
+    
+    cb(null, file.originalname )
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
 router.post('/', upload.single('file'), function (req, res) {
     res.status(200).send({Message : 'Uploaded'})
     console.log(req.body)

dc506ca4c63e67e85f1909a15b30470c2fa04525
haseebismail703 <haseebismail703@gmail.com>
2024-06-13 21:10:27 +0500

    new

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import chalk from 'chalk'
 import 'dotenv/config'
 import main from './Routes/main.js';
 import mongoose from './db/data.js';
+import { createServer } from 'http';
+import {Server} from 'socket.io'
 const app = express();
 const PORT = process.env.PORT;
 
@@ -17,17 +19,33 @@ db.once('open',()=>{
 app.use(express.json());
 app.use(cors())
 
-app.use('/', (req, res, next) => {
-    if (req.query.apikey === '123') {
-        next()
-        console.log(req.query.apikey)
-    } else {
-        res.status(500).send({ status: 500, Mesaage: 'Not allowed' })
-    }
-})
+// app.use('/', (req, res, next) => {
+//     if (req.query.apikey === '123') {
+//         next()
+//         console.log(req.query.apikey)
+//     } else {
+//         res.status(500).send({ status: 500, Mesaage: 'Not allowed' })
+//     }
+// })
 
 app.use('/api', main);
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+const httpserver = createServer(app)
+
+const io = new Server(httpserver,{ cors : '*'})
+
+io.on("connection", (socket) => {
+    console.log("Socket connect",socket.id)
+    socket.on("add",(data)=>{
+        console.log("data",data);
+        
+    })
+  });
+  httpserver.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-});
+  })

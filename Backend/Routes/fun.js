ed603cdfa08f57873ca81cecb5dd34818090e628
haseebismail703 <haseebismail703@gmail.com>
2024-06-18 22:04:04 +0500

    new

--- Backend/Routes/fun.js
+++ Backend/Routes/fun.js
@@ -1,40 +1,40 @@
-// import express from 'express'
-// const router = express.Router()
+import express from 'express'
+const router = express.Router()
 
 
-// let users = [
-//     {
-//         id : 1,
-//         name: 'Haseeb',
-//         class: '12'
-//     },
+let users = [
+    {
+        id : 1,
+        name: 'Haseeb',
+        class: '12'
+    },
 
-//     {
-//         id :2,
-//         name: 'Haseeb 2',
-//         class: '123'
-//     }
-// ]
-
-
-
-// router.post('/', async (req, res) => {
-//     console.log(req.body)
-//     users.push({...req.body})
-//     res.status(200).send(users)
-//   })
-
-//   router.get('/all', async (req, res) => {
-//     console.log(req.body)
-//     res.status(200).send(users)
-//   })
+    {
+        id :2,
+        name: 'Haseeb 2',
+        class: '123'
+    }
+]
 
 
-//   router.get('/:id', async (req, res) => {
-//     console.log(req.params)
-//     const index = users.findIndex(v => v.id === Number(req.params.id))
-//     users.splice(index,1)
-//     res.status(200).send({Message : "Delete succes"})
-//   })
 
-// export default router
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    users.push({...req.body})
+    res.status(200).send(users)
+  })
+
+  router.get('/all', async (req, res) => {
+    console.log(req.body)
+    res.status(200).send(users)
+  })
+
+
+  router.get('/:id', async (req, res) => {
+    console.log(req.params)
+    const index = users.findIndex(v => v.id === Number(req.params.id))
+    users.splice(index,1)
+    res.status(200).send({Message : "Delete succes"})
+  })
+
+export default router

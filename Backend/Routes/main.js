574335f72797f01ce240a3a855dedea740a24518
haseebismail703 <haseebismail703@gmail.com>
2024-06-19 23:04:41 +0500

    new

--- Backend/Routes/main.js
+++ Backend/Routes/main.js
@@ -2,11 +2,11 @@ import express from 'express'
 import user from './user.js'
 import upload from './upload.js'
 import delet from './delet.js'
-import  fun from './fun.js'
+// import  fun from './fun.js'
 
 const router = express.Router()
 router.use('/user',user)
 router.use('/upload',upload)
 router.use('/delete',delet)
-router.use('/fun',fun)
+// router.use('/fun',fun)
 export default router 
--- Backend/server.js
+++ Backend/server.js
@@ -9,11 +9,11 @@ import {Server} from 'socket.io'
 const app = express();
 const PORT = process.env.PORT;
 
-// const db = mongoose.connection
-// db.on('error',console.error.bind(console,'Error connection'))
-// db.once('open',()=>{
-//     console.log(chalk.green('Db connected'));
-// })
+const db = mongoose.connection
+db.on('error',console.error.bind(console,'Error connection'))
+db.once('open',()=>{
+    console.log(chalk.green('Db connected'));
+})
 
 
 app.use(express.json());
